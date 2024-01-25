5360e81f060168bf3b66d83d4ae2f0fd525876e6
vishalbrdr <biradarvishal290@gmail.com>
2024-01-25 22:14:37 +0530

    user controller

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { PORT } from "./constants.js";
+import { app } from "./app.js";
+dotenv.config({ path: "./env" });
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log(err);
+      throw err;
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB Connection failed !!!", err);
+  });
